e31fde666c39e6fb6c7c8777b0bfdf704e2d1a46
Shashidarreddy19 <shashidarreddy.630@gmail.com>
2025-11-29 00:51:52 +0530

    port number added

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,6 +8,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'fertilizer_inventory',
+    port: process.env.DB_PORT || 8081,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
