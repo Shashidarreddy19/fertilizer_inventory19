488fadb177fd0a444299015c90725d0e1a722a78
Shashidarreddy19 <shashidarreddy.630@gmail.com>
2025-11-29 19:29:45 +0530

    changed db data

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,11 +4,11 @@ dotenv.config();
 
 // Create the connection pool with enhanced settings
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DB_NAME || 'fertilizer_inventory',
-    port: process.env.DB_PORT || 8081,
+    host: 'myapp-db.cvkuyac060bj.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Fertilizer2025',
+    database:'fertilizer_inventory',
+    port: 3036,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
